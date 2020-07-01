5b088fca833e3ab573cbfbc0222f66bd13edafdf
MagicRom06 <romainhenry60@gmail.com>
2020-07-01 11:09:42 +0200

    add charge set + search function

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BooksListView, BooksDetailView
+from .views import BooksListView, BooksDetailView, charge, SearchResultListView
 
 urlpatterns = [
     path('', BooksListView.as_view(), name='books_list'),
     path('<uuid:pk>', BooksDetailView.as_view(), name='books_detail'),
+    path('charge/', charge, name='charge'),
+    path('search/', SearchResultListView.as_view(), name='search_results'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView, DetailView
 from .models import Book
 from django.conf import settings
+import stripe
+from django.shortcuts import render
+from django.db.models import Q
+
+stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
 
 # Create your views here.
@@ -20,3 +25,26 @@ class BooksDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['stripe_key'] = settings.STRIPE_TEST_PUBLISHABLE_KEY
         return context
+
+
+def charge(request):
+    if request.method == 'POST':
+        charge = stripe.Charge.create(
+            amount=599,
+            currency='usd',
+            description=request.POST['description'],
+            source=request.POST['stripeToken']
+        )
+        return render(request, 'charge.html')
+
+
+class SearchResultListView(ListView):
+    model = Book
+    context_object_name = 'books_list'
+    template_name = 'search_result.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
